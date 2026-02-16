df00c58dee28733ac6577db3f7c9b3b0f204c4a2
6504062610056yanisa <s6504062610056@email.kmutnb.ac.th>
2026-02-17 01:04:47 +0700

    Update show number of no.1 stat

--- lab8no2.cpp
+++ lab8no2.cpp
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 int checkscore(char std[]);
+int checkq1(char ans[][10]);
+
 char keys[10] = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
 
 int main()
@@ -14,12 +16,14 @@ int main()
         {'A', 'B', 'D', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
         {'D', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
         {'B', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
-        {'E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}};
+        {'E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}
+    };
 
-    for (i = 0; i < 8; i++)
-    {
+    for (i = 0; i < 8; i++) {
         printf("std %d => %d\n", i + 1, checkscore(ans[i]));
     }
+
+    printf("Question1 correct => %d \n", checkq1(ans));
 
     return 0;
 }
@@ -27,12 +31,21 @@ int main()
 int checkscore(char std[])
 {
     int score = 0;
-    for (int i = 0; i < 10; i++)
-    {
-        if (std[i] == keys[i])
-        {
+    for (int i = 0; i < 10; i++) {
+        if (std[i] == keys[i]) {
             score++;
         }
     }
     return score;
+}
+
+int checkq1(char ans[][10])
+{
+    int count = 0;
+    for (int i = 0; i < 8; i++) {
+        if (ans[i][0] == keys[0]) {
+            count++;
+        }
+    }
+    return count;
 }
